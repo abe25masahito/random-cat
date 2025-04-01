fddf0f39d46f967199611e017cd25c3fb76ddb90
ABE Masahito <abeche@detch.info>
2025-04-01 22:37:40 +0900

    change pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
   };
   return (
     <div className={styles.page}>
-      <button onClick={handleClick}>他のにゃんこも見る</button>
+      <button onClick={handleClick}>One more Cat!</button>
       <div className={styles.frame}>{loading || <img src={imageUrl} className={styles.img} />}</div>
     </div>
   );
